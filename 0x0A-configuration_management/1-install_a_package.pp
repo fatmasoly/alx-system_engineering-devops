a24cef9c454bd310fcb0c9ba6074b231613f1c7f
root <root@DESKTOP-CTTDNQL.localdomain>
2024-03-22 23:14:20 +0300

    fixxx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Installing flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install flask
+package { 'python3-pip':
+  ensure   => installed,
+}
+
+exec { 'install-flask':
+  command => 'pip3 install flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin', '/usr/sbin'],
 }

a09f676c5060fcebdaa3d18b7a92e615077ac141
Fatma <fatmacivil@hotmail.com>
2024-04-22 14:58:52 +0300

    jj

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This Python script to export data in the JSON format."""
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"

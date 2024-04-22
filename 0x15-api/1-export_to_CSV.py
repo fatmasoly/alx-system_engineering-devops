fe1c56f21c00e852acef601df29adf98f63d6384
Fatma <fatmacivil@hotmail.com>
2024-04-22 14:01:25 +0300

    fxxx

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
-"""
-Module to interact with a REST API and manipulate data
-
-"""
-
-
+"""Python script that, using this REST API, for a given employee ID
+and saves it to a CSV file."""
 import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user = requests.get("{}users/{}".format(url, user_id)).json()
-    todos = requests.get("{}todos?userId={}".format(url, user_id)).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
 
-with open("{}.csv".format(user_id), "w") as csvfile:
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    [writer.writerow([user_id, user.get('username'),
-                      todo.get('completed'), todo.get('title')])
-     for todo in todos]
+# #!/usr/bin/python3
+# """
+# Module to interact with a REST API and manipulate data
+
+# """
+
+
+# import csv
+# import requests
+# import sys
+
+
+# if __name__ == "__main__":
+#     url = "https://jsonplaceholder.typicode.com/"
+#     user_id = sys.argv[1]
+
+#     user = requests.get("{}users/{}".format(url, user_id)).json()
+#     todos = requests.get("{}todos?userId={}".format(url, user_id)).json()
+
+# with open("{}.csv".format(user_id), "w") as csvfile:
+#     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+#     [writer.writerow([user_id, user.get('username'),
+#                       todo.get('completed'), todo.get('title')])
+#      for todo in todos]

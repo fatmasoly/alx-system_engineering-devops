6ca7ad5df3159b4e053b0ac18237aa605bbdaf7f
Fatma <fatmacivil@hotmail.com>
2024-04-22 13:47:34 +0300

    hhh

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    todos = requests.get("{}todos?userId={}".format(url, user_id)).json()
+
+with open("{}.csv".format(user_id), "w") as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    [writer.writerow([user_id, user.get('username'),
+                      todo.get('completed'), todo.get('title')])
+     for todo in todos]

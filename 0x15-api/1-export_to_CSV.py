3ab0fe749d565fe791cec0e9a49de991b4b46ed8
Fatma <fatmacivil@hotmail.com>
2024-04-22 14:05:09 +0300

    dd

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,39 +6,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user_id = sys.argv[1]
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    todos = requests.get("{}todos?userId={}".format(url, user_id)).json()
 
-# #!/usr/bin/python3
-# """
-# Module to interact with a REST API and manipulate data
-
-# """
-
-
-# import csv
-# import requests
-# import sys
-
-
-# if __name__ == "__main__":
-#     url = "https://jsonplaceholder.typicode.com/"
-#     user_id = sys.argv[1]
-
-#     user = requests.get("{}users/{}".format(url, user_id)).json()
-#     todos = requests.get("{}todos?userId={}".format(url, user_id)).json()
-
-# with open("{}.csv".format(user_id), "w") as csvfile:
-#     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-#     [writer.writerow([user_id, user.get('username'),
-#                       todo.get('completed'), todo.get('title')])
-#      for todo in todos]
+with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    [writer.writerow([user_id, user.get('username'),
+                      todo.get('completed'), todo.get('title')])
+     for todo in todos]

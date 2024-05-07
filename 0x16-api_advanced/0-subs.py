06d83d74e4b91f7c356273603401a09307926625
Fatma <fatmacivil@hotmail.com>
2024-05-07 16:51:28 +0300

    ggg

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API and
-    returns the number of subscribers"""
+""" Module that queries the Reddit API and
+returns the number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
     if subreddit is None:
         return 0
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
-    if response.status_code == 200:
+    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
+    x = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
+    if x.status_code == 200:
         try:
-            return response.json()['data']['subscribers']
+            return x.json()['data']['subscribers']
         except:
             return 0
     else:
         return 0
+
+
+
+# #!/usr/bin/python3
+# """Module that queries the Reddit API and
+#     returns the number of subscribers"""
+# import requests
+
+
+# def number_of_subscribers(subreddit):
+#     """Returns the number of subscribers for a given subreddit"""
+#     if subreddit is None:
+#         return 0
+#     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+#     response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
+#     if response.status_code == 200:
+#         try:
+#             return response.json()['data']['subscribers']
+#         except:
+#             return 0
+#     else:
+#         return 0

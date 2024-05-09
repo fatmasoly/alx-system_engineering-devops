db5b2096c63641fbf4034e881efa3f958c0deb63
fatma soliman <fatmacivil@hotmail.com>
2024-05-09 01:40:10 -0700

    hhh

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API"""
+"""Module that queries the Reddit API and
+    returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    headers = {'User-Agent': 'alxAPI'}
-    res = requests.get(
-        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-        headers=headers,
-        allow_redirects=False
-    )
-
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
+    """Returns a tuple indicating success and
+       the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return True, subscribers
+    elif response.status_code == 404:
+        return False, 0
     else:
-        return 0
+        return False, 0
+

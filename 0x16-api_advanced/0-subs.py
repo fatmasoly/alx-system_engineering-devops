b6f7927ef50e80384e29d2dba21df630aac47650
fatma soliman <fatmacivil@hotmail.com>
2024-05-09 01:42:02 -0700

    hate this task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,17 +5,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns a tuple indicating success and
-       the number of subscribers for a given subreddit"""
+    """Returns the number of subscribers for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data.get('data', {}).get('subscribers', 0)
-        return True, subscribers
-    elif response.status_code == 404:
-        return False, 0
+        return subscribers
     else:
-        return False, 0
-
+        return 0

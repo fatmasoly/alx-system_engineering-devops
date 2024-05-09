3d262bcb7dd7ece028a9ae51f2887dbd73b15a97
fatma soliman <fatmacivil@hotmail.com>
2024-05-09 01:58:51 -0700

    ggg

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""This Module is to recurse function"""
+"""
+Recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
+"""
 
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Returns a list of titles of all hot articles for a given subreddit."""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'limit': 100, 'after': after}
-    headers = {'User-Agent': 'custom user-agent'}
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
 
-    response = requests.get(url,
-                            params=params,
-                            headers=headers,
-                            allow_redirects=False)
+    - If not a valid subreddit, return None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
 
-    if response.status_code == 404:
-        return None
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
 
-    data = response.json().get('data', {})
-    for post in data.get('children', []):
-        hot_list.append(post.get('data').get('title'))
-
-    if data.get('after'):
-        return recurse(subreddit, hot_list, data.get('after'))
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
-        return hot_list
+        return None

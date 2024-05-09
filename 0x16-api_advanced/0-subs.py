d8e223bc565fc052027f952f985337e49b1644ad
fatma soliman <fatmacivil@hotmail.com>
2024-05-09 01:31:05 -0700

    fx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API and
-    returns the number of subscribers"""
+""" Module that queries the Reddit API and
+returns the number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    if subreddit is None:
+        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
+    x = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
+    if x.status_code == 200:
+        try:
+            return x.json()['data']['subscribers']
+        except:
+            return 0
     else:
         return 0
